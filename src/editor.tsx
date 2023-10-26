9eeed18a6b61e1856ee17e0e3d639bf71bca540d
Leemacintyre <macintyrelee@gmail.com>
2023-10-26 15:55:38 +0100

    first commit

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "jsoneditor/dist/jsoneditor.css";
-import JSONEditor from "jsoneditor";
+import type JSONEditor from "jsoneditor";
 import type { JSONEditorOptions } from "jsoneditor";
 import { api } from "~@/utils/api";
 
